b6685a04b03c5697bb1f00cc06dc83757dc81476
LetTTGACO <LetTTGACO@users.noreply.github.com>
2023-07-20 15:55:43 +0000

    feat: 配置 LetTTGACO/elog-docs@a5ee1d7ca5110a25b119cdbdf531b74fa769656a

---
+++ assets/yuque_start.md.ab9808b4.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,a as e}from"./app.74b00e3a.js";const g=JSON.parse('{"title":"快速开始","description":"","frontmatter":{},"headers":[],"relativePath":"yuque/start.md","lastUpdated":1689868526000}'),l={name:"yuque/start.md"},p=e("",14),o=[p];function t(c,r,i,d,h,C){return n(),a("div",null,o)}const u=s(l,[["render",t]]);export{g as __pageData,u as default};
